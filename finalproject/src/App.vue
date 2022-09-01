fc31688599d31001d0c82bbcd04bf8808a10f30c
Diana Jurca <diana.roxana.lazar@gmail.com>
2022-09-01 21:53:22 +0200

    he mirado las grabadas de supabase, netlify, pinia

--- finalproject/src/App.vue
+++ finalproject/src/App.vue
@@ -1,10 +1,22 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+ <div class="app">
+    <section class=""
+  </div>
 </template>
+
+<script>
+import { ref, onMounted, computed, watch } from 'vue';
+const todos = ref([]);
+const name = ref('');
+const input_content = ref('');
+const input_category = ref(null);
+
+const todos_asc = computed(() => todos.value.sort((a,b) => {
+  return b.createdAt - b.createdAt
+}));
+
+</script>
+
 
 <style>
 #app {
